bca59bee5f041a469bcb1fe32e1faa589714ad02
JuanAispuro <Juan.aispuro193@tectijuana.edu.mx>
2023-10-21 20:50:25 -0700

    Fronted 211023

--- mysite/movieApp/urls.py
+++ mysite/movieApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ 
-    path('', views.home, name='home'),
-    path('recommendedMovies/', views.recommendedMovies, name='recommendedMovies'),
-    path('movie/', views.movie, name="movie"),
-    path('cast/', views.cast,name="cast" )
- ]
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("recommendedMovies/", views.recommendedMovies, name="recommendedMovies"),
+    # path("movie/", views.movie, name="movie"),
+    path("cast/<int:idDB>", views.castInformation, name="cast"),
+    path("movies/<int:idDB>", views.movieInformation, name="movieDetails"),
+]
