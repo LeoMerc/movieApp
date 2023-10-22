f132fbb47c7beb85510dd9370765cb921c8658ee
JuanAispuro <Juan.aispuro193@tectijuana.edu.mx>
2023-10-22 13:07:06 -0700

    Arreglos finales y ya se ve los reviews

--- mysite/movieApp/views.py
+++ mysite/movieApp/views.py
@@ -21,29 +21,30 @@ from .forms import MovieReviewForm
 # Create your views here.
 
 
-     
 class Register(views.View):
-        def get(self, request):
-            form = UserRegisterForm()
-            return render(request, "register.html", {"form": form})
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, "register.html", {"form": form})
 
-        def post(self, request):
-            # render form with POST body data
-            form = UserRegisterForm(request.POST)
+    def post(self, request):
+        # render form with POST body data
+        form = UserRegisterForm(request.POST)
 
-            # check if form is vaild
-            if form.is_valid():
-                new_user = form.save()
+        # check if form is vaild
+        if form.is_valid():
+            new_user = form.save()
 
-                # if valid, save form and authenticate user
-                new_user = authenticate(
-                    username=form.cleaned_data['username'],
-                    password=form.cleaned_data['password1'])
+            # if valid, save form and authenticate user
+            new_user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password1"],
+            )
 
-                login(request, new_user)
+            login(request, new_user)
 
-            # redirect when finished
-            return redirect('home')
+        # redirect when finished
+        return redirect("home")
+
 
 def home(request):
     return render(
@@ -114,20 +115,22 @@ def movieInformation(request, idDB):
     movie = Movie.objects.get(tmdb_id=idDB)
     print("----------")
     print(movie)
-    print(movie.genresfK)
-    print(movie.credits.all())
+    print(movie.tmdb_id)
+    reviews = MovieReview.objects.filter(moviefK=movie)
+    print("----------")
+    studio = Studio.objects.filter(name=movie.studiofK)
+    print(studio)
     # movieC = Movie.credits.get(tmdb_id=idDB)
     # movie.credits.through.objects.filter(movie=movie)
     return render(
         request,
         "movie.html",
-        {"movie": movie},
+        {"movie": movie, "reviews": reviews, "studio": studio},
     )
 
 
 def castInformation(request, idDB):
     person = Person.objects.get(id=idDB)
-    print(person)
     # pit = MovieCredit.objects.get(tmdb_id=idDB)
     movieCredits = MovieCredit.objects.filter()
     # print(movieCredits)
@@ -142,34 +145,38 @@ def castInformation(request, idDB):
     )
 
 
+# def getReviews(request, idDB):
+#     movie = Movie.objects.get(tmdb_id=idDB)
+#     reviews = MovieCredit.objects.filter(movie=movie)
+
+#     print(reviews)
+#     return render(request, "movie.html", {"review": reviews})
+
 
 class formReview(views.View):
-
     def get(self, request, idDB, userID):
-            movie = Movie.objects.get(tmdb_id=idDB)
-            user = User.objects.get(id=userID)
-            form = MovieReviewForm(initial={'moviefK': movie, 'userfK': user})
-            return render(request, "review.html", {"form": form})
+        movie = Movie.objects.get(tmdb_id=idDB)
+        user = User.objects.get(id=userID)
+        form = MovieReviewForm(initial={"moviefK": movie, "userfK": user})
+        return render(request, "review.html", {"form": form})
 
     def post(self, request, idDB, userID):
-            movie = Movie.objects.get(tmdb_id=idDB)
-            user = User.objects.get(id=userID)
-            print(movie)
-            form = MovieReviewForm(request.POST)
-            if form.is_valid():
-                review = form.save(commit=False)
-                review.moviefK = movie
-                review.userfK = user
-                review.save()
-                return redirect('movieDetails/' + str(idDB))  
-            
-            # # render form with POST body data
-            # form = MovieReviewForm(request.POST)
-            # # check if form is vaild
-            # if form.is_valid():
-            #     form.save()
+        movie = Movie.objects.get(tmdb_id=idDB)
+        user = User.objects.get(id=userID)
+        print(movie)
+        form = MovieReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.moviefK = movie
+            review.userfK = user
+            review.save()
+            return redirect(request.META.get("HTTP_REFERER"))
 
-               
+    # # render form with POST body data
+    # form = MovieReviewForm(request.POST)
+    # # check if form is vaild
+    # if form.is_valid():
+    #     form.save()
 
-            # # redirect when finished
-            # return redirect('movieDetails')
+    # # redirect when finished
+    # return redirect('movieDetails')
